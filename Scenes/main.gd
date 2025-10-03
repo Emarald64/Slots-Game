c4b802b84f81962e730be67ac5fcbc820ffad6da
Alex Giller <alexgiller@proton.me>
2025-10-02 23:31:30 -0400

    more slots and button to stop and start slots

---
+++ Scenes/main.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var coins:=0
+var currentSlot:=0
+
+
+func _on_stop_pressed() -> void:
+	if $"Button Lockout".is_stopped():
+		if currentSlot==3:
+			print('start')
+			for i in range(3):
+				get_node("Slots/Slot"+str(i)).startWithDelay((i/5.0)+randf_range(0,0.15))
+			$Button.text='Stop'
+			$"Button Lockout".wait_time=1.8
+			currentSlot=0
+		else:
+			get_node("Slots/Slot"+str(currentSlot)).stop()
+			if currentSlot==2:
+				$Button.text='Start'
+			$"Button Lockout".wait_time=1.5 if currentSlot==2 else 0.1
+			currentSlot+=1
+		$"Button Lockout".start()
--- Scenes/slot.gd
+++ Scenes/slot.gd
@@ -1,9 +1,14 @@
 extends Node2D
 
 enum {HEART,DIAMOND,CLUB,SPADE,DIE,GEM,SEVEN,ORANGE,BAR,STAR}
+enum actions {NONE,STOP,START}
 
-var acceleration:=0
-var velocity:=-128
+var acceleration:=0.0
+var maxSpeed:=-175
+var velocity:float=maxSpeed
+var stopping:=false
+var starting:=false
+var currentAction:= actions.NONE
 var currentIcons:=[HEART,DIAMOND,CLUB,SPADE,GEM,ORANGE,STAR,HEART,DIAMOND,CLUB,SPADE,DIE,HEART,DIAMOND,CLUB,SPADE,GEM,ORANGE,STAR,HEART,DIAMOND,CLUB,SPADE,DIE,SEVEN]
 
 
@@ -13,20 +18,51 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	#if(stopping and velocity>-2):
+		#$Clip/Icons.position.y=roundf($Clip/Icons.position.y/18)*18
+	velocity+=acceleration*delta/2
+	if velocity>=0 and stopping:
+		acceleration=0
+		velocity=0
+	elif velocity<=maxSpeed and starting:
+		acceleration=0
+		starting=false
+		
 	$Clip/Icons.position.y+=velocity*delta
-	$Clip/Icons.position.y=fposmod($Clip/Icons.position.y+(len(currentIcons)+1.5)*18,float(len(currentIcons)*18))-(len(currentIcons)+1.5)*18
-
+	$Clip/Icons.position.y=fposmod($Clip/Icons.position.y+(len(currentIcons)+2.5)*18,float(len(currentIcons)*18))-(len(currentIcons)+2.5)*18
+	velocity+=acceleration*delta/2
+	
 func updateIcons() -> void:
 	#clear out old icons
 	for sprite in $Clip/Icons.get_children():
 		sprite.free()
 	# add new icons
-	for iconIdx in range(len(currentIcons)+3):
+	for iconIdx in range(len(currentIcons)+5):
 		var sprite:=Sprite2D.new()
 		sprite.texture=preload("res://assets/icons.png")
 		sprite.hframes=10
 		sprite.frame=currentIcons[iconIdx%len(currentIcons)]
 		sprite.position=Vector2(0,iconIdx*18)
 		$Clip/Icons.add_child(sprite)
+
+func stop() -> void:
+	acceleration=(velocity**2)/(2*(54+fposmod($Clip/Icons.position.y,18.0)))
+	stopping=true
+
+func startWithDelay(delay:=0.0):
+	currentAction=actions.START
+	$"Action delay".wait_time=delay if delay>0 else randf()
+	$"Action delay".start()
+
+func start() -> void:
+	acceleration=maxSpeed
+	stopping=false
+	starting=true
 	
+func doAction() -> void:
+	match currentAction:
+		actions.START:
+			start()
+		actions.STOP:
+			stop()
